6991e1d6676de493bac59baf39f685ce3ea31d95
Gabe Lopez <git.unfixed408@passmail.net>
2025-02-25 14:08:28 -0600

    adding data

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\Api\V1\UsersController;
+
 use App\Http\Controllers\AuthController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
@@ -17,7 +19,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:santum')->apiResource('tickets', TicketController::class);
+// http://localhost:8000/api/
+// univseral resource locator
+// tickets
+// users
+
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('users', UsersController::class);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
